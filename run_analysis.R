b3f9494dbe79193323de9d0d253fb5f0abbac202
Max Trubetskoy <handcrab@gmail.com>
2014-09-20 04:40:39 +0400

    create tidy data set with the average of each variable

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,4 @@
-# doesn't work with the current data.table verions 1.9.2: cann't read files that are missing values in first row
-#install_github("Rdatatable/data.table", build_vignettes=FALSE)
-
-#library(data.table)
-#setwd('./')
+#setwd('.')
 # Download and unzip file if it's not exists
 file.url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 dir.name = "UCI HAR Dataset"
@@ -10,51 +6,60 @@ dir.name = "UCI HAR Dataset"
 # TODO check if the files exists within the folder
 if (!file.exists(dir.name)) {   
     tmp.file = tempfile()
-    #file.arhive.name = paste("household_power_consumption.txt", '.zip')
 
     download.file(file.url, tmp.file, method='curl')
     unzip(tmp.file)
-    #file.remove(file.arhive.name)
     unlink(tmp.file)
 }
 
-# features.txt
-# activity_labels.txt
-
-
 # 1. Merges the training and the test sets to create one data set.
-#features.data = fread(file.path(dir.name,'features.txt'))
-#labels.data = fread(file.path(dir.name,'activity_labels.txt'))
-
-#setnames(labels.data, c('id', 'activity'))
-#setnames(features.data, c('id', 'feature'))
-
-# x.train = fread(file.path(dir.name, 'train', 'X_train.txt'), header=F)
-# x.test = fread(file.path(dir.name, 'test', 'X_test.txt'), header=F)
-
-labels.data = read.table(file.path(dir.name,'activity_labels.txt'))
-colnames(labels.data) = c("id", "activity")
-
-features.data = read.table(file.path(dir.name,'features.txt'))
-colnames(features.data) <- c("id", "feature")
-
 x.train = read.table(file.path(dir.name, 'train', 'X_train.txt'))
 x.test = read.table(file.path(dir.name, 'test', 'X_test.txt'))
-
-# Merge the training and the test data
 x.data = rbind(x.train, x.test)
-remove(x.train, x.test)
+
+features.data = read.table(file.path(dir.name,'features.txt'))
+colnames(features.data) = c("id", "feature")
+
 colnames(x.data) = features.data$feature
+remove(x.train, x.test, features.data)
 
-# Extract only the measurements on the mean and standard deviation for each measurement.
-tmp.cols = grep("mean\\(|std\\(", features.data$feature)
-x.data = x.data[, tmp.cols]
+# TODO same work for x(feature) and y(activity) data sets
+y.train = read.table(file.path(dir.name, 'train', 'y_train.txt'))
+y.test = read.table(file.path(dir.name, 'test', 'y_test.txt'))
+y.data = rbind(y.train, y.test)
+remove(y.train, y.test)
+colnames(y.data) = "activity"
 
-# makes column names more tidy
-# colnames(x.data) = gsub("[-\\(\\)]", '', colnames(x.data))
-colnames(x.data) = gsub("[^[:alpha:][:digit:]]", '', colnames(x.data))
-colnames(x.data) = tolower(colnames(x.data))
+# subject data
+subj.train = read.table(file.path(dir.name, 'train', 'subject_train.txt'))
+subj.test = read.table(file.path(dir.name, 'test', 'subject_test.txt'))
+subject.data = rbind(subj.train, subj.test)
+remove(subj.train, subj.test)
 
+colnames(subject.data) = "subject"
+subject.data$subject = factor(subject.data$subject) 
 
+# merge training, test, subject data
+tidy.data = cbind(subject.data, y.data, x.data)
+remove(subject.data, y.data, x.data)
 
+# 2. Extract only the measurements on the mean and standard deviation for each measurement.
+tidy.data = subset(tidy.data, select=c(1,2,grep("mean\\(|std\\(", colnames(tidy.data))))
 
+# 3. Uses descriptive activity names to name the activities in the data set
+activity.labels = read.table(file.path(dir.name,'activity_labels.txt'))
+colnames(activity.labels) = c("id", "activity")
+
+tidy.data$activity = factor(tidy.data$activity, labels=activity.labels$activity) 
+remove(activity.labels)
+
+# 4.   Appropriately labels the data set with descriptive variable names. 
+colnames(tidy.data) = gsub("[^[:alpha:][:digit:]]", '', colnames(tidy.data)) # "[-\\(\\)]"
+colnames(tidy.data) = tolower(colnames(tidy.data))
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+measured.data = subset(tidy.data, select=-c(subject, activity))
+tidy.data.avg = aggregate(measured.data, by=list(subject=tidy.data$subject, activity=tidy.data$activity), FUN=mean, na.rm=T)
+remove(measured.data)
+
+write.table(tidy.data.avg, file="tidy.txt", row.names=FALSE, quote=FALSE)
